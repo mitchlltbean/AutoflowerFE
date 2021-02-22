f333f5a7bcb1194faf43842e78f5e421ff0f1cdd
Zach <zdspringer99@gmail.com>
2021-02-22 14:22:02 -0800

    created initial buttons and groups

---
+++ src/components/Buttons/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+import Button from "semantic-ui-react";
+
+function Buttons() {
+  return (
+    <div id="btnHolder">
+      <Button.Group vertical>
+        <Button>1</Button>
+        <Button>4</Button>
+        <Button>7</Button>
+        <Button>Clear</Button>
+      </Button.Group>
+      <Button.Group vertical>
+        <Button>2</Button>
+        <Button>5</Button>
+        <Button>8</Button>
+        <Button>0</Button>
+      </Button.Group>
+      <Button.Group vertical>
+        <Button>3</Button>
+        <Button>6</Button>
+        <Button>9</Button>
+        <Button>Check</Button>
+      </Button.Group>
+    </div>
+  );
+}
+
+export default Buttons;
