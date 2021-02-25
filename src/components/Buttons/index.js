7131d53e728be06b1b5f8ba02ba8c1469e655661
V Guardado <auricia.guardado@gmail.com>
2021-02-25 10:39:08 -0800

    password input  to owner page

--- src/components/Buttons/index.js
+++ src/components/Buttons/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, Container, Grid } from "semantic-ui-react";
+import { Button, Container } from "semantic-ui-react";
 import "semantic-ui-css/semantic.min.css";
 import "./style.css";
 
@@ -15,8 +15,6 @@ class Buttons extends React.Component {
     console.log(numeral);
     keycodeCopy += numeral;
 
-    // if (keycodeCopy.length < 5 ) { }
-
     this.setState({ keycode: keycodeCopy });
   };
 
@@ -26,32 +24,39 @@ class Buttons extends React.Component {
     this.setState({ keycode: "" });
   };
 
-  //Method to verify the input is correct
+  //Method to verify the input is correct/meets length requirements.
+  //Is it correct? 
   checkNum = () => {
     const keycode = this.state.keycode.length;
     if (keycode < 5) {
       console.log("Make it longer!");
+      return false;
     } else if (keycode > 5) {
       console.log("Make it shorter!");
+      return false; 
     } else {
       console.log("Thats about right!");
+      window.location.href ="/owner";
     }
   };
 
-  //Cap the max-length (if/else)
+
+  //Let the user know if the password is correct or incorrect. 
+  
 
   render() {
     return (
-      <Container id="landing">
+      <div id="landing">
         <form>
           <input
             id="input"
             value={this.state.keycode}
             onChange={this.checkNum}
+            type ="password"
           />
         </form>
 
-        <div id="btnHolder" className="flex-container">
+        <Container id="btnHolder" className="flex-container">
 
           <Button.Group vertical>
 
@@ -146,133 +151,14 @@ class Buttons extends React.Component {
               onClick={(e) => this.checkNum(e, "value")}
               className="numLock"
               id="check"
+              // onChange ={handleInputChange}
             >
               Check
             </Button>
           </Button.Group>
 
-
-
-
-          {/* <Grid id="btnHolder" className="three column grid">
-
-            <Button.Group className="row">
-
-              <Button
-                className="num column"
-                onClick={(e) => this.feelNum(e, "value")}
-                value="1"
-              >
-                1
-            </Button>
-
-              <Button
-                className="num column"
-                onClick={(e) => this.feelNum(e, "value")}
-                value="4"
-              >
-                4
-             </Button>
-
-              <Button
-                className="num column"
-                onClick={(e) => this.feelNum(e, "value")}
-                value="7"
-              >
-                7
-            </Button>
-
-            </Button.Group>
-
-            <Button.Group className="row">
-
-              <Button
-                onClick={(e) => this.clearNum(e, "value")}
-                className="numLock column"
-                id="clear"
-              >
-                Clear
-              </Button>
-
-              <Button
-                className="num column"
-                onClick={(e) => this.feelNum(e, "value")}
-                value="2"
-              >
-                2
-               </Button>
-
-              <Button
-                className="num column"
-                onClick={(e) => this.feelNum(e, "value")}
-                value="5"
-              >
-                5
-               </Button>
-
-            </Button.Group>
-
-            <Button.Group className="row">
-              <Button
-                className="num column"
-                onClick={(e) => this.feelNum(e, "value")}
-                value="8"
-              >
-                8
-              </Button>
-
-              <Button
-                className="num column"
-                onClick={(e) => this.feelNum(e, "value")}
-                value="0"
-              >
-                0
-              </Button>
-
-              <Button
-                className="num column"
-                onClick={(e) => this.feelNum(e, "value")}
-                value="3"
-              >
-                3
-              </Button>
-
-            </Button.Group>
-
-            <Button.Group className="row">
-
-              <Button
-                className="num column"
-                onClick={(e) => this.feelNum(e, "value")}
-                value="6"
-              >
-                6
-              </Button>
-
-
-              <Button
-                className="num column"
-                onClick={(e) => this.feelNum(e, "value")}
-                value="9"
-              >
-                9
-              </Button>
-
-
-              <Button
-                onClick={(e) => this.checkNum(e, "value")}
-                className="numLock column"
-                id="check"
-              >
-                Check
-               </Button>
-
-            </Button.Group>
-
-          </Grid> */}
-
-      </div>
       </Container>
+      </div>
     );
   }
 }
