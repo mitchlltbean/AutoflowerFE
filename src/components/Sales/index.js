d573f7f9679ed180dc8c61932b69ecbdc5b9dfb5
V Guardado <auricia.guardado@gmail.com>
2021-02-26 10:52:57 -0800

    added a card

--- src/components/Sales/index.js
+++ src/components/Sales/index.js
@@ -1,12 +1,62 @@
 import React from "react";
-import {} from "semantic-ui-react";
+import { Card } from "@material-ui/core";
+import useStyles from "./sideBarStyles";
 import "./style.css";
 
-class Sales extends React.Component {
+//perhaps card or Buttons interface ?
 
-}
+const useStyles = makeStyles({
+    root: {
+      minWidth: 275,
+    },
+    bullet: {
+      display: 'inline-block',
+      margin: '0 2px',
+      transform: 'scale(0.8)',
+    },
+    title: {
+      fontSize: 14,
+    },
+    pos: {
+      marginBottom: 12,
+    },
+  });
+  
+  export default function SimpleCard() {
+    const classes = useStyles();
+    const bull = <span className={classes.bullet}>•</span>;
+  
+    return (
+      <Card className={classes.root}>
+        <CardContent>
 
+          <Typography className={classes.title} color="textSecondary" gutterBottom>
+            Word of the Day
+          </Typography>
 
-//perhaps ard or Buttons interface ?
+          <Typography variant="h5" component="h2">
+            be{bull}nev{bull}o{bull}lent
+          </Typography>
+
+          <Typography className={classes.pos} color="textSecondary">
+            adjective
+          </Typography>
+
+          <Typography variant="body2" component="p">
+            well meaning and kindly.
+            <br />
+            {'"a benevolent smile"'}
+          </Typography>
+          
+        </CardContent>
+
+        <CardActions>
+
+          <Button size="small">Learn More</Button>
+
+        </CardActions>
+      </Card>
+    );
+  }
 
 export default Sales; 
--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,7 +1,6 @@
 import React from "react";
 import "./style.css";
 import Buttons from "../components/Buttons";
-import DataTable from "../components/DataTable";
 
 function Landing() {
   return (
@@ -9,7 +8,6 @@ function Landing() {
         
         <Buttons />
 
-      <DataTable />
       
       </div>
   );
