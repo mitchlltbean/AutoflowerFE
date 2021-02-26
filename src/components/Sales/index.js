a8581db886c16781346b9fe55d6a2d9d92e7bb89
V Guardado <auricia.guardado@gmail.com>
2021-02-26 10:09:48 -0800

    getting ready to push

---
+++ src/components/Sales/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import {} from "semantic-ui-react";
+import "./style.css";
+
+class Sales extends React.Component {
+
+}
+
+
+//perhaps ard or Buttons interface ?
+
+export default Sales; 
