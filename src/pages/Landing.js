d6335edaf886ecabd866956f701b3640d871d549
V Guardado <auricia.guardado@gmail.com>
2021-02-26 13:09:31 -0800

    create card to sales page

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,18 +1,13 @@
 import React from "react";
 import "./style.css";
 import Buttons from "../components/Buttons";
-import DataTable from "../components/DataTable";
-import Sales from "../components/Sales";
+
 
 function Landing() {
   return (
       <div>
         
         <Buttons />
-
-        <DataTable />
-
-        <Sales />
 
       
       </div>
