d45db662a5c1ab96f2afe9ca221e8724782e18df
Zach <zdspringer99@gmail.com>
2021-02-26 22:02:36 -0800

    created mock-data for new cards that have been added to Employee.js

--- src/pages/Employees.js
+++ src/pages/Employees.js
@@ -1,12 +1,23 @@
 import React from "react";
 import Menu from "../components/Menu";
+import EmpCard from "../components/EmpCard";
+import employees from "../employees.json";
 
 function Employees() {
   return (
     <div id="employeePage">
       <Menu />
       <div id="empWrapper">
-        <h1>Employees Page</h1>
+        <h1>Employees</h1>
+
+        {employees.map((employee) => (
+          <EmpCard
+            name={employee.name}
+            emp_id={employee.emp_id}
+            login={employee.login}
+            is_manager={employee.is_manager}
+          />
+        ))}
       </div>
     </div>
   );
