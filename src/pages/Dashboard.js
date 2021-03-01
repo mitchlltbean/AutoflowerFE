9122fee9005b2ba4b132722b85f5cd26d3e48685
V Guardado <auricia.guardado@gmail.com>
2021-02-28 22:06:08 -0800

    footer

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,11 +1,18 @@
 import React from "react";
+import Footer from "../components/Footer";
 import Menu from "../components/Menu";
 
 function Dashboard() {
   return (
     <div id="dashboardPage">
+
       <Menu />
+      
+      <Footer /> 
+      
+
     </div>
+
   );
 }
 
