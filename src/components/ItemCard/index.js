d351f68f51063b72064634153b3a1e009caf3e8c
Zach <zdspringer99@gmail.com>
2021-02-28 18:36:21 -0800

    auto formatter got to it

--- src/components/ItemCard/index.js
+++ src/components/ItemCard/index.js
@@ -9,68 +9,69 @@ import {
 import { makeStyles } from "@material-ui/core/styles";
 // import useStyles from "./sideBarStyles";
 import "./style.css";
-import { Autorenew } from "@material-ui/icons";
 
-//Card styling below 
-
+//Card styling below
 const useStyles = makeStyles({
-    root: {
-      minWidth: 275,
-      maxWidth: 300,
-      width: 'calc(100% / 4)',
-      margin: '1rem',
-      position:'relative',
-      justifyContent: 'center',
-      backgroundColor: '#90BE6D',
-      flexGrow: 1,
-      textAlign: 'center',
-    
-    },
+  root: {
+    minWidth: 275,
+    maxWidth: 300,
+    width: "calc(100% / 4)",
+    margin: "1rem",
+    position: "relative",
+    justifyContent: "center",
+    backgroundColor: "#90BE6D",
+    flexGrow: 1,
+    textAlign: "center",
+  },
 
-    bullet: {
-      display: 'inline-block',
-      margin: '0 2px',
-      transform: 'scale(0.8)',
-    },
-    title: {
-      fontSize: 14,
-    },
-    pos: {
-      marginBottom: 12,
-    },
-  });
-  
-  export default function SimpleCard(props) {
-    const classes = useStyles();
-    const bull = <span className={classes.bullet}>•</span>;
-  
-    return (
-      <Card className={classes.root}>
-        <CardContent>
+  bullet: {
+    display: "inline-block",
+    margin: "0 2px",
+    transform: "scale(0.8)",
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+});
 
-          <Typography className={classes.title} color="textSecondary" gutterBottom>
-            {props.item}
-          </Typography>
+export default function SimpleCard(props) {
+  const classes = useStyles();
+  const bull = <span className={classes.bullet}>•</span>;
 
-          <Typography className={classes.title} color="textSecondary" gutterBottom>
-            {props.description}
-          </Typography>
+  return (
+    <Card className={classes.root}>
+      <CardContent>
+        <Typography
+          className={classes.title}
+          color="textSecondary"
+          gutterBottom
+        >
+          {props.item}
+        </Typography>
 
-          <Typography variant="h5" component="h2">
-            {props.price}
-          </Typography>
+        <Typography
+          className={classes.title}
+          color="textSecondary"
+          gutterBottom
+        >
+          {props.description}
+        </Typography>
 
-          <Typography className={classes.pos} color="textSecondary">
-            {props.instock}
-          </Typography>
+        <Typography variant="h5" component="h2">
+          {props.price}
+        </Typography>
 
-        </CardContent>
+        <Typography className={classes.pos} color="textSecondary">
+          {props.instock}
+        </Typography>
+      </CardContent>
 
-        <CardActions>
-
-          <Button size="small">Learn More</Button>
-
-        </CardActions>
-      </Card>
-    );
-  }
+      <CardActions>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
+  );
+}
