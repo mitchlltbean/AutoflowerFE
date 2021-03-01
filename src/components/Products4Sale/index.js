42774173a4c344f4974fd9a090141f8382ac5b82
Zach <zdspringer99@gmail.com>
2021-02-28 18:57:22 -0800

    started adding product cards to the order page

--- src/components/Products4Sale/index.js
+++ src/components/Products4Sale/index.js
@@ -1,126 +1,40 @@
 import React from "react";
-import { Button, Typography } from "@material-ui/core";
-// import SimpleCard from "../ItemCard";
+import { Button, Card, CardContent, Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
-// import img from "marijuana-leaf.png";
 
 const useStyles = makeStyles({
-  root: {
-    minWidth: 275,
-    maxWidth: 300,
-    width: "calc(100% / 4)",
-    height: "4em",
-    margin: "1rem",
-    position: "relative",
-    justifyContent: "center",
-    backgroundColor: "#90BE6D",
-    flexGrow: 1,
-    textAlign: "center",
-  },
   title: {
-    fontSize: 14,
-  },
-  buttoncontainer: {
-    backgroundColor: "lightgray",
-    width: "40%",
-    height: "100%",
-  },
-  buttonrow: {
-    display: "flex",
-    float: "right",
-    width: "33%",
+    fontSize: 12,
   },
 });
 
-export default function Products4Sale() {
+export default function Products4Sale(props) {
   const classes = useStyles();
 
   return (
     <div>
-      <Button className={classes.root} container spacing={1}>
-        <Typography
-          variant="h2"
-          className={classes.title}
-          color="textSecondary"
-          gutterBottom
-        >
-          Flower
-        </Typography>
-      </Button>
-
-      <Button className={classes.root} container spacing={1}>
-        <Typography
-          variant="h2"
-          className={classes.title}
-          color="textSecondary"
-          gutterBottom
-        >
-          Concentrates
-        </Typography>
-      </Button>
-
-      <Button className={classes.root} container spacing={1}>
-        <Typography
-          variant="h2"
-          className={classes.title}
-          color="textSecondary"
-          gutterBottom
-        >
-          Edibles
-        </Typography>
-      </Button>
+      {/* //TODO Add Dropdown Menu Here!!!*/}
+      {/* /* Category Buttons. They need to render appropriate products. */}
 
       {/* The Order Panel with the products buttons. These need formatting, autopopulate, on click events*/}
-      <div className={classes.buttoncontainer}>
-        <div className={classes.buttonrow}>
-          <Button
-            className={classes.root}
-            //   onClick={}
-          >
-            Product #1
-          </Button>
-
-          <Button
-            className={classes.root}
-            // onClick={}
-          >
-            Product #2
-          </Button>
-
-          <Button
-            className={classes.root}
-            // onClick={}
-          >
-            Product #3
-          </Button>
-        </div>
-      </div>
+      <Card>
+        <CardContent>
+          <img src={props.img} alt={props.img} />
+          <Typography gutterBottom className={classes.title} varient="body1">
+            Item: {props.item}
+          </Typography>
+          <Typography gutterBottom className={classes.title}>
+            Description: {props.description}
+          </Typography>
+          <Typography gutterBottom className={classes.title}>
+            Price: {props.price}
+          </Typography>
+          <Typography gutterBottom className={classes.title}>
+            In Stock: {props.instock}
+          </Typography>
+        </CardContent>
+      </Card>
     </div>
     // class InventoryPanel extends React.Component
-    // Attempting to render products as cards =========================
-
-    //     render(){
-    //     const classes = useStyles();
-    //     return (
-    //       <div>
-    //      {products.map((products) => {
-    //           console.log(products);
-    //           return (
-    //       <Card className={classes.root} container spacing={1} key={products.id}>
-
-    //         <CardContent>
-    //           <Typography variant="h2" className={classes.title} color="textSecondary" gutterBottom>
-    //             {products.category}
-    //           </Typography>
-
-    //         </CardContent>
-
-    //       </Card>
-    //     )})}
-    //     </div>
-    //     );
-    //   }
-    // }
-    /* Category Buttons. They need to render appropriate products. */
   );
 }
--- src/pages/Orders.js
+++ src/pages/Orders.js
@@ -2,11 +2,32 @@ import React from "react";
 import Grid from "@material-ui/core/Grid";
 import InventoryPanel from "../components/InventoryPanel";
 import Products4Sale from "../components/Products4Sale";
+import products from "../products.json";
+import { makeStyles } from "@material-ui/core/styles";
+import "./style.css";
 
 // import Menu from "../components/Menu";
 import "./style.css";
 
+const useStyles = makeStyles({
+  root: {
+    minWidth: 275,
+    maxWidth: 300,
+    width: "calc(100% / 4)",
+    height: "8em",
+    margin: "1rem",
+    position: "relative",
+    justifyContent: "center",
+    backgroundColor: "gold",
+    flexGrow: 1,
+    display: "flex",
+    flexFlow: "row",
+  },
+});
+
 export default function Orders() {
+  const classes = useStyles();
+
   return (
     <div>
       {/* Side Bar Menu commented out for this page */}
@@ -16,9 +37,25 @@ export default function Orders() {
           <h1>Orders</h1>
           <InventoryPanel />
         </div>
-        <div>
-          <Products4Sale />
-        </div>
+        <Grid
+          container
+          direction="row"
+          justify="space-evenly"
+          xs={8}
+          spacing={3}
+          alignItems="baseline"
+          className={classes.root}
+        >
+          {products.map((product) => (
+            <Products4Sale
+              img={product.img}
+              item={product.item}
+              description={product.description}
+              price={product.price}
+              instock={product.instock}
+            />
+          ))}
+        </Grid>
       </Grid>
     </div>
   );
