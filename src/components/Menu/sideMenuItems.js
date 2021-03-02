6b15b4dea27dd405e2654bcfb5be93b76a3f80e6
Zach <zdspringer99@gmail.com>
2021-03-02 12:24:33 -0800

    redid the navbar to have a responsive menu

--- src/components/Menu/sideMenuItems.js
+++ src/components/Menu/sideMenuItems.js
@@ -1,29 +1,50 @@
-// When user makes an account hordcode evryone as a user and figure out how to make these users an admin
-// name, url, adminOnly: true/false
+import React from "react";
+import * as FaIcons from "react-icons/fa";
+import * as AiIcons from "react-icons/ai";
+import * as IoIcons from "react-icons/io";
 
-export default {
-  data: [
-    {
-      // Tried imorting the component itself to the page, only made the list vanish
-      name: "Employees",
-      url: "/employees",
-    },
-    {
-      name: "Sales",
-      url: "/sales",
-    },
-    {
-      name: "Manage Products",
-      url: "/products",
-    },
-    {
-      name: "Statistics(Later)",
-      url: "/stats",
-    },
+export const sideMenuItems = [
+  {
+    name: "Home",
+    url: "/dashboard",
+    icon: <IoIcons.IoMdPeople />,
+    className: "nav-text",
+  },
+  {
+    // Tried imorting the component itself to the page, only made the list vanish
+    name: "Employees",
+    url: "/employees",
+    icon: <IoIcons.IoMdPeople />,
+    className: "nav-text",
+  },
+  {
+    name: "Sales",
+    url: "/sales",
+    icon: <IoIcons.IoMdPeople />,
+    className: "nav-text",
+  },
+  {
+    name: "Manage Products",
+    url: "/products",
+    icon: <IoIcons.IoMdPeople />,
+    className: "nav-text",
+  },
+  {
+    name: "Statistics(Later)",
+    url: "/stats",
+    icon: <IoIcons.IoMdPeople />,
+    className: "nav-text",
+  },
 
-    {
-      name: "Orders",
-      url: "/orders",
-    },
-  ],
-};
+  {
+    name: "Orders",
+    url: "/orders",
+    icon: <IoIcons.IoMdPeople />,
+    className: "nav-text",
+  },
+];
+
+// export default {
+//   data: [
+//   ],
+// };
